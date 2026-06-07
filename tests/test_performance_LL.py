4a12cd388dcedf06da99115654247a7f286a8e05
Bonbia <bonbia75@gmail.com>
2026-06-07 16:44:23 +0200

    Ajt Test_Perf et Conformity pour Lunar_Lander

---
+++ tests/test_performance_LL.py
@@ -0,0 +1,21 @@
+from pufferlib.ocean.lunar_lander.Lunar-lander import test_performance
+import time
+import numpy as np
+import mo_gymnasium as mo_gym
+env = mo_gym.make("LunarLander-v2")
+env.reset(seed=0)
+
+steps = 0
+start = time.time()
+while time.time() - start < 10:
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+    if terminated or truncated:
+        env.reset()
+    steps += 1
+
+elapsed = time.time() - start
+print("----Testing the implemented environment performance----")
+test_performance(timeout=10)
+print("----Testing the original environment performance----")
+print(f"MO-Gymnasium SPS: {steps / elapsed:.0f}")
